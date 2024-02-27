63baef70707f22513f3e4bb8e256037ba79320ca
MatheusThierri <matheusthierri7@gmail.com>
2024-02-27 11:57:50 -0300

    feat: previsão do tempo

---
+++ script.js
@@ -0,0 +1,25 @@
+const key = "5ab675e2d86f5f5dbbcc724e79154243"
+
+function colocarDadosNaTela(dados) {
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".temp").innerHTML = "Temperatura: " + Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description;
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector(".max").innerHTML = "Máxima: " + Math.floor(dados.main.temp_max) + "°C"; 
+    document.querySelector(".min").innerHTML = "Mínima: " + Math.floor(dados.main.temp_min) + "°C"; 
+}
+
+async function buscarCidade(cidade){
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    
+    colocarDadosNaTela(dados);
+ 
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".input-cidade").value;
+
+    buscarCidade(cidade)
+}
